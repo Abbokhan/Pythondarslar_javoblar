720504b40d7ec1a78e597a083e04b37b24908642
Abbos0661 <88732305+Abbokhan@users.noreply.github.com>
2022-11-26 22:44:38 +0500

    keyingi vazifam

---
+++ vazifam-26.py
@@ -0,0 +1,29 @@
+buyurtmalar=[]
+n=1
+while True:
+    buyurtma=input(f"{n}-Buyurtmangizni kiriting!\n>>>")
+    buyurtmalar.append(buyurtma)
+    savol=input("Buyurtma qilishda davom etaszmi? (xa\yo'q)\n>>>")
+    n+=1
+    if savol != 'xa':
+        break
+
+
+e_bozor={}
+
+print("Elektron bozorimiz uchun mahsulot va mahsulot narhlarini kiritamiz!")
+while True:
+    mahsulot=input("Mahsulotni kiriting!\n>>>")
+    narh=int(input(f"{mahsulot.title()}ning narhini kiriting!\n>>>"))
+    e_bozor[mahsulot]=narh
+    savol=input("Yana mahsulot haqida ma'lumot kiritasizmi? (xa\yo'q)\n>>>")
+    if savol != 'xa':
+        break
+
+while buyurtmalar:
+    buyurtma=buyurtmalar.pop()
+    if buyurtma in e_bozor.keys():
+        narh=e_bozor[buyurtmakoka]
+        print(f"Bizda {buyurtma} mavjud, uning narhi {narh} so'm!")
+    else:
+        print(f"Bizda {buyurtma} mavjud emas!")
